4c9e22af40ef0fc5c447df6419759532c7d2ffb9
mklocampo <mkl.ocampo@gmail.com>
2026-09-05 12:01:42 -0600

    initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,92 @@
+import React, { useEffect }  from 'react';
+
+import { Link, useNavigate } from 'react-router-dom'
+//import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+
+export default function Navbar() {
+
+    const { isLoggedIn, setIsLoggedIn, userName, setUserName } = useAppContext();
+
+    const navigate=useNavigate();
+    useEffect(() => {
+        const authTokenFromSession = sessionStorage.getItem('auth-token');
+        const nameFromSession = sessionStorage.getItem('name');
+        if (authTokenFromSession) {
+            if(isLoggedIn && nameFromSession) {
+              setUserName(nameFromSession);
+            } else {
+              sessionStorage.removeItem('auth-token');
+              sessionStorage.removeItem('name');
+              sessionStorage.removeItem('email');
+              setIsLoggedIn(false);
+            }
+        }
+    },[isLoggedIn, setIsLoggedIn, setUserName])
+    
+    const handleLogout=()=>{
+        sessionStorage.removeItem('auth-token');
+        sessionStorage.removeItem('name');
+        sessionStorage.removeItem('email');
+        setIsLoggedIn(false);
+        navigate(`/app`);
+    }
+
+    //Added for navigation display
+    const currentUrl = window.location.href;
+    const menuLinks = document.querySelectorAll('.nav-item a');
+    menuLinks.forEach(link => {
+        if (link.href === currentUrl) {
+            link.classList.add('active');
+        }else{
+            link.classList.remove('active');
+        }
+    });
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="navbar-brand">
+                <Link className="nav-link" to="/app">
+                    <img src="/images/gifts.png" ></img>
+                    <label>GiftLink</label>
+                </Link>       
+            </div>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    {/* Task 1: Add links to Home and Gifts below*/}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/home.html">Home</a> {/* Link to home.html */}
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" to="/app">Gifts</Link>
+                    </li>
+                    <li className="nav-item">
+	                    <Link className="nav-link" to="/app/search">Search</Link>
+                    </li>
+                    <ul className="navbar-nav ml-auto">
+                        {isLoggedIn ? (
+                            <>
+                                <li className="nav-item">  
+                                    <Link className="nav-link profile" to="/app/profile">Welcome, {userName}</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link" onClick={handleLogout}>Logout</button>
+                                </li>
+                            </>
+                        ) : (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/app/login">Login</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/app/register">Register</Link>
+                                </li>
+                            </>
+                        )
+                        }
+                    </ul>
+                </ul>
+            </div>
+        </nav>
+    );
+}
